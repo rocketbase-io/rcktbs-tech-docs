5f2dcfc2970628984611e7b538ecf86cd93450e0
Marten <marten@rocketbase.io>
2025-06-12 08:52:50 +0200

    feat: wip started first draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,25 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		resolve: {
+			alias: {
+				'@': new URL('./src', import.meta.url).pathname
+			}
+		}
+	},
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'rcktbs tech',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/rocketbase-io/rcktbs-tech-docs' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Intro', slug: 'guides/intro' },
+						{ label: 'Folder Structure', slug: 'guides/folder-structure' },
 					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
---
+++ src/content/docs/guides/intro.md
@@ -0,0 +1,6 @@
+---
+title: Intro
+description: Was hat es hier mit den Guides und co auf sich
+---
+
+Lasst uns einen Platz für unsere Konzepte und zum Erstellen von Leitlinien...
